d7627d35bb066ba861891bd95f71b2b9542a9252
T Ghelere <ghelere@outlook.com>
2018-09-11 20:51:40 -0300

    Comitão por segurança 5;

--- wp-content/themes/softheme/includes/pages/atendimento.php
+++ wp-content/themes/softheme/includes/pages/atendimento.php
@@ -4,23 +4,25 @@
         <div class="col-md-4 col">
             <div class="col-md-12 one">
                 <h3>Consulta</h3>
-                <p>Atendimento presencial em consultório Nutricional com suporte de avaliação Bioimpedância (avaliação de composição corporal)  e mentoria Health Coach.</p>
-                <p>Segunda a sexta-feira <br />8h às 18h30 <br />Av. Nóbrega, 536 - Zona 4 - Maringá</p>
+                <p>Atendimento presencial em consultório Nutricional com suporte de avaliação Bioimpedância (avaliação de composição corporal) e análise e mentoria Health Coach.</p>
+                <p>Segunda a sexta-feira, <br />das 9h às 18h <br /><?= ADDRESS; ?></p>
+                <p>solicite agendamento</p>
             </div>
         </div>
         <div class="col-md-4 col">
             <div class="col-md-12 two">
-                <h3>Programa Cardápio em Casa</h3>
+                <h3>Programa Cardápio em Casa - assinatura de alimentação saudável personalizada</h3>
                 <p>A partir do plano nutricional feito em consulta, a Nutri Paula entrega as refeições em casa, conforme planejado.</p>
                 <p>Cardápio variado e equilibrado desenvolvido por ela e preparado com o maior cuidado e higiene por sua equipe.</p>
-                <p>Verificar disponibilidade em consulta Disponível somente para Maringá-PR</p>
+                <p>Consultar disponibilidade</p>
+                <p>Disponível somente para Maringá-PR</p>
             </div>
         </div>
         <div class="col-md-4 col">
             <div class="col-md-12 three">
                 <h3>Cursos</h3>
-                <p>Periodicamente a Nutri Paula Ayres Ferreira ministra cursos dentro da temática de alimentação saudável, como técnicas de preparo de alimentos por exemplo. Fique atento na página de Novidades</p>
-                <p>Se não quiser perder os lançamentos, inscreva seu email.  Prometemos não enviar spams ;)</p>
+                <p>Periodicamente a Nutri Paula Ayres Ferreira ministra cursos dentro da temática de alimentação saudável, como técnicas de preparo de alimentos por exemplo. Fique atento na página de <u>Cursos</u></p>
+                <p>Se não quiser perder os lançamentos, inscreva seu e-mail aqui Prometemos não enviar spams ;)</p>
                 <p>Nome</p>
                 <p>E-mail</p>
             </div>
--- wp-content/themes/softheme/includes/pages/sobre.php
+++ wp-content/themes/softheme/includes/pages/sobre.php
@@ -3,27 +3,14 @@
         <h1>Nutrição</h1>
         <p class="sub">Confira abaixo abaixo um pouco sobre o trabalho da Nutri Paula Ayres Ferreira para cada uma das áreas:</p>
         <h3>Emagrecimento saudável e ganho de massa magra</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam condimentum semper scelerisque. Fusce ac venenatis
-            massa, vel suscipit dolor. Etiam molestie ut dui ut vulputate. Pellentesque et nullam.</p>
-        <h3>Detox / Desintoxicação </h3>
-        <p>Detox é um termo bastante comentado sem muitos fundamentos geralmente, mas o objetivo é eliminar toxinas
-            instaladas no organismo através de uma dieta natural e restritiva. O corpo sente rapidamente os benefícios
-            e responde através de bem-estar, vitalidade e desinchaço.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam condimentum semper scelerisque. Fusce ac venenatis massa, vel suscipit dolor. Etiam molestie ut dui ut vulputate. Pellentesque et nullam.</p>
+        <h3>Detox / Desintoxicação</h3>
+        <p>Detox é um termo bastante comentado sem muitos fundamentos geralmente, mas o objetivo é eliminar toxinas instaladas no organismo através de uma dieta natural e restritiva. O corpo sente rapidamente os benefícios e responde através de bem-estar, vitalidade e desinchaço.</p>
         <h3>Nutrição funcional / suplementação</h3>
-        <p>Estar nutrido é algo além de se alimentar. Dificilmente uma dieta é composta por todos os nutrientes necessários
-            que a longo prazo geram prejuízos. A nutrição funcional atua como a integração de processos físicos,
-            metabólicos e psicológicos, tornando cada caso único, individual em busca do equilíbrio e da saúde do
-            indivíduo
-        </p>
+        <p>Estar nutrido é algo além de se alimentar. Dificilmente uma dieta é composta por todos os nutrientes necessários que a longo prazo geram prejuízos. A nutrição funcional atua como a integração de processos físicos, metabólicos e psicológicos, tornando cada caso único, individual em busca do equilíbrio e da saúde do indivíduo</p>
         <h3>Nutrição - distúrbios alimentares</h3>
-        <p>Um transtorno alimentar se caracteriza pelos hábitos alimentares incorretos associados a danos a saúde, como
-            anorexia, bulimia e compulsão. A reeducação alimentar nesse caso é essencial para restabelecer energia,
-            compor o equilíbrio químico e consequentemente promover melhor percepção do próprio hábito alimentar
-            - nutrição comportamental. </p>
+        <p>Um transtorno alimentar se caracteriza pelos hábitos alimentares incorretos associados a danos a saúde, como anorexia, bulimia e compulsão. A reeducação alimentar nesse caso é essencial para restabelecer energia, compor o equilíbrio químico e consequentemente promover melhor percepção do próprio hábito alimentar - nutrição comportamental.</p>
         <h3>Vegetarianismo e veganismo</h3>
-        <p>Com a popularização da dieta vegetariana e vegana, diversas pessoas acabam aderindo ao vegetarianismo e cortando
-            nutrientes essenciais da dieta por desconhecimento. Assim, além de uma avaliação nutricional para verificação
-            de anemia e deficiência de vitaminas, a Nutri Paula trabalha com orientação de preparo de alimentos e
-            diversificação de cardápio.</p>
+        <p>Com a popularização da dieta vegetariana e vegana, diversas pessoas acabam aderindo ao vegetarianismo e cortando nutrientes essenciais da dieta por desconhecimento. Assim, além de uma avaliação nutricional para verificação de anemia e deficiência de vitaminas, a Nutri Paula trabalha com orientação de preparo de alimentos e diversificação de cardápio.</p>
     </div>
 </div>
